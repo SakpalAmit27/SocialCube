6406c26878a7b75d5d4baabee9f6671740ef46f8
Sakpal Amit <amitsakpal744@gmail.com>
2024-10-18 18:18:53 +0530

    added views page and response when someone visits

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
 # Create your views here.
+
+#creating the functionm for index 
+
+def index(request):
+    return HttpResponse('<h1>Welcome to SocialCube</h1 ')
+    
